db6152e608086208b74dc0e7df999bc971a858b6
CARLOS ALBERTO ROBERTS MARTINEZ <1085639@est.intec.edu.do>
2019-07-08 23:04:19 -0400

    borre

--- Estudiantes/Form1.Designer.cs
+++ Estudiantes/Form1.Designer.cs
@@ -150,7 +150,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(209, 327);
+            this.button1.Location = new System.Drawing.Point(192, 333);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(210, 51);
             this.button1.TabIndex = 10;
